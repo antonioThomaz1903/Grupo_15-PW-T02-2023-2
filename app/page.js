f8e9bac03f5e6e7c7a08e4dad59978b6347416f8
Antonio Thomaz <antoniotor987@gmail.com>
2023-10-15 21:46:48 -0400

    atualização

--- app/page.js
+++ app/page.js
@@ -2,8 +2,18 @@
 export default function Home() {
   return (
     <main className='container centro'>
-      <div>Bom dia</div>
-      <div>Acesse localhost:3000/sobre</div>
+      <div className="container-texto">
+        <div>Login</div>
+        <textarea className="texto"></textarea>
+        <div>Senha</div>
+        <textarea className="texto"></textarea>
+        <br></br>
+        <button className="texto-mini">Esqueci a senha</button>
+        <br></br>
+        <button className="botao laranja mt-10">Entrar</button>
+        <br></br>
+        <button className="botao verde mt-10">Cadastrar</button>
+      </div>
     </main>
   )
 }
